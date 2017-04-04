f560e7b724661d6a779245866f9a04a7a9b54d6f
Anthony Moyi <Anthony Moyi>
2017-04-04 09:26:38 +0300

    added the recurssive case

--- Day1/recurssive.py
+++ Day1/recurssive.py
@@ -8,4 +8,6 @@ def replicate_recur(times, data):
      # base case
     if times == 0 or times < 0 0r type(times) == type(1/3):
         return solution
-        
+
+    # recurssive case    
+    solution.append(data)
